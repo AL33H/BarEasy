9350b5833ae3799da54edd3358fb3281467697f3
Aleff Remberto <aleff.remberto@hotmail.com>
2024-07-06 22:42:05 -0300

    initial commit

---
+++ backend/barEasy/src/main/java/br/com/bareasy/BarEasyApplication.java
@@ -0,0 +1,13 @@
+package br.com.bareasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarEasyApplication.class, args);
+    }
+
+}
---
+++ backend/barEasy/src/test/java/br/com/bareasy/BarEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bareasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
